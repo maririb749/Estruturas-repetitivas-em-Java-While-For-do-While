6015ffa099be8f398419285f699bcc60660f134c
Mariana Santos <maririb51@gmail.com>
2022-06-14 16:53:05 +0100

    Dois_numeros_while

---
+++ src/Main/while_1.java
@@ -0,0 +1,33 @@
+package Main;
+
+import java.util.Scanner;
+
+public class while_1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int x, y;
+
+		System.out.println("Digite dois numeros:");
+		x = sc.nextInt();
+		y = sc.nextInt();
+
+		while (x != y) {
+			if (x > y) {
+				System.out.println("DECRESCENTE!");
+			} else {
+				System.out.println("CRESCENTE!");
+
+			}
+			System.out.println("Digite outros dois numeros: ");
+			x = sc.nextInt();
+			y = sc.nextInt();
+
+		}
+
+		sc.close();
+	}
+
+}
