b4d4007dfbd9bf507fe82a492c9038fa04143582
Mariana Santos <maririb51@gmail.com>
2022-06-14 17:11:40 +0100

    media_idades_while

---
+++ src/Main/while_2.java
@@ -0,0 +1,47 @@
+package Main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class while_2 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Faça um programa para ler um número indeterminado de dados, contendo cada um,
+		 * a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém
+		 * um valor de idade negativa. Calcular e imprimir a idade média deste grupo de
+		 * indivíduos. Se for entrado um valor negativo na primeira vez, mostrar a
+		 * mensagem "IMPOSSIVEL CALCULAR".
+		 */
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int idade, npessoas;
+		double soma,media;
+
+		System.out.println("Digite as idades: ");
+		idade = sc.nextInt();
+		
+		if (idade < 0) {
+			System.out.println("IMPOSSIVEL CALCULAR");
+		}
+		else {
+			 soma = 0;
+			 npessoas = 0;
+			 while (idade >=0) {
+				 soma = soma + idade;
+				 npessoas++;
+				 idade = sc.nextInt();
+			 }
+			 media = soma / npessoas;
+			 
+			 System.out.printf("MEDIA = %.2f\n", media);
+		}
+
+		sc.close();
+
+	}
+
+}
