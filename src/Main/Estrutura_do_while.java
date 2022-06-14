56aca9410e4003baa1fdd456a9a6537c0472353b
Mariana Santos <maririb51@gmail.com>
2022-06-14 16:24:38 +0100

    estrutura_do_while

---
+++ src/Main/Estrutura_do_while.java
@@ -0,0 +1,34 @@
+package Main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Estrutura_do_while {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Fazer um programa para ler uma temperatura em Celsius e mostrar o equivalente
+		 * em Fahrenheit.Perguntar se o usuário deseja repetir(s/n). Caso o usuário
+		 * digite "s", repetir o programa.
+		 */
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		char resp;
+
+		do {
+			System.out.print("Digitea a temperatura em Celsius: ");
+			double C = sc.nextDouble();
+			double F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+			System.out.print("Deseja repetir (s/n)?");
+			resp = sc.next().charAt(0);
+		} while (resp != 'n');
+
+		sc.close();
+
+	}
+
+}
--- src/Main/estrutura_for.java
+++ src/Main/estrutura_for.java
@@ -6,6 +6,12 @@ public class estrutura_for {
 
 	public static void main(String[] args) {
 
+		/*
+		 * Fazer um programa que lê um valor inteiro N e depois N números inteiros. Ao
+		 * final, mostrar a soma dos N números lidos.
+		 * 
+		 */
+
 		Scanner sc = new Scanner(System.in);
 
 		int N = sc.nextInt();
