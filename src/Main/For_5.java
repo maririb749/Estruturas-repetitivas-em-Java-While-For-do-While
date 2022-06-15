5715851ccdaee8aaacda6b89462234048822d49b
Mariana Santos <maririb51@gmail.com>
2022-06-15 14:42:23 +0100

    par_impar

---
+++ src/Main/For_5.java
@@ -0,0 +1,53 @@
+package Main;
+
+import java.util.Scanner;
+
+public class For_5 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Leia um valor inteiro N. Este valor será a quantidade de números inteiros que
+		 * serão lidos em seguida. Para cada valor lido, mostre uma mensagem dizendo se
+		 * este valor lido é PAR ou IMPAR, e também se é POSITIVO ou NEGATIVO. No caso
+		 * do valor ser igual a zero (0), seu programa deverá imprimir apenas NULO.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+
+		int n, valor;
+
+		System.out.print("Quantos numeros voce vai digitar?");
+		n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("Digite um numero:");
+			valor = sc.nextInt();
+
+			if (valor == 0) {
+				System.out.println("NULO");
+			} 
+			else {
+				if (valor % 2 == 0) {
+					System.out.print("PAR");
+				} 
+				else {
+					System.out.print("IMPAR");
+
+				}
+				if (valor > 0) {
+					System.out.println(" POSITIVO");
+
+				} 
+				else {
+					System.out.println(" NEGATIVO");
+				}
+
+			}
+		}
+
+		sc.close();
+
+	}
+
+}
