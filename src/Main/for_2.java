7e7098aa584d9474b74e5c7caa5d8422da4ac179
Mariana Santos <maririb51@gmail.com>
2022-06-15 12:29:02 +0100

    soma impares

---
+++ src/Main/for_2.java
@@ -0,0 +1,41 @@
+package Main;
+
+import java.util.Scanner;
+
+public class for_2 {
+
+	public static void main(String[] args) {
+		/*
+		 * Leia 2 valores inteiros X e Y (em qualquer ordem). A seguir, calcule e mostre
+		 * a soma dos números impares entre eles.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+
+		int x, y, soma, troca;
+
+		System.out.println("Digite dois numeros; ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+
+		if (x > y) {
+			troca = x;
+			x =y;
+			y = troca;
+		}
+
+		soma = 0;
+		for (int i = x + 1; i < y; i++) {
+			if (i % 2 != 0) {
+				soma = soma + i;
+
+			}
+		}
+
+		System.out.printf("SOMA DOS IMPARES = %d\n", soma);
+
+		sc.close();
+
+	}
+
+}
